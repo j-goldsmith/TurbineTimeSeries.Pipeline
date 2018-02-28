b1dcfc9bd0758fcdf936a04c3a2e4dc7660f0909
Michael Galarnyk <michaelgalarnyk@Michaels-MacBook-Pro.local>
2018-02-27 22:21:39 -0800

    Changed order of setup.py to make it install first and then configure s3

--- setup.py
+++ setup.py
@@ -2,18 +2,18 @@ from setuptools import setup, find_packages
 import subprocess
 
 def setup_package():
-	setup(name="TurbineTimeSeries",
-      	      version="0.1",
-      	      packages=['TurbineTimeSeries'],
-      	      install_requires=[
+    setup(name="TurbineTimeSeries",
+      	  version="0.1",
+      	  packages=['TurbineTimeSeries'],
+      	  install_requires=[
             	    'sqlalchemy',
             	    'sklearn',
             	    'pandas',
             	    'awscli'
-              ],
-              zip_safe=False)
+          ],
+          zip_safe=False)
 
 
 if __name__ == '__main__':
-    subprocess.call("setup.sh", shell =True)
     setup_package()
+    subprocess.call("setup.sh", shell =True)
