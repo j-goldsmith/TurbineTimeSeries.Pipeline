017ef1ee39b81baa1318a478db4d3fb7f256328c
j-goldsmith <jared.goldsmith@gmail.com>
2018-05-05 11:22:39 -0700

    update pipeline definition

--- TurbineTimeSeries/train.py
+++ TurbineTimeSeries/train.py
@@ -1,10 +1,15 @@
+from datetime import timedelta
 from sklearn.pipeline import Pipeline, FeatureUnion
 from TurbineTimeSeries.transformations import (
     DropNA,
     DropCols,
     DropSparseCols,
     PCA,
-    StandardScaler
+    StandardScaler,
+    PartitionByTime,
+    FlattenPartitionedTime,
+    KMeansLabels,
+    RoundTimestampIndex
 )
 from TurbineTimeSeries.exports import (
     png_pca_eigenvalues_as_tags,
@@ -15,20 +20,36 @@ from TurbineTimeSeries.exports import (
 
 
 def init(package_model_config, data_query, exporter, verbose=False):
+    fleetwide = Pipeline([
+                ('FleetwidePCA', PCA(exporter=exporter)
+                    .after_transform([
+                        png_pca_variance_explained_curve,
+                        png_pca_eigenvalues_as_tags])),
+                ('Partition', PartitionByTime(
+                    col=0,
+                    partition_span=timedelta(minutes=30))
+                 ),
+                ('KMeans', KMeansLabels(exporter=exporter, n_clusters=225)),
+                ('Flatten',FlattenPartitionedTime())
+          ])
+
+
     pipeline = Pipeline([
         ('DropCols', DropCols(package_model_config.ignored)),
         ('DropSparseCols', DropSparseCols(.1)),
         # ('DropSparsePackages',DropSparsePackages(1000)),
-        (
-            'DropNA',
-            DropNA(exporter=exporter)
-               # .after_transform([csv_cleaned_data])
+        ('DropNA',DropNA(exporter=exporter)
+            # .after_transform([csv_cleaned_data])
         ),
+        ('RoundTimeStamps', RoundTimestampIndex(to='10min')),
         ('StandardScaler', StandardScaler()),
-        ('PCA', PCA(exporter=exporter).after_transform([
-            png_pca_variance_explained_curve,
-            png_pca_eigenvalues_as_tags])),
-        # ('End',None)
+        ('FleetwidePCA', fleetwide)
+        #FeatureUnion([
+        #    ,
+            #('ByPackagePCA',by_package)
+        #])
+
+
         # ('FleetwideClusters', FleetwideClusters()),
         # ('PackageClusters', PackageClusters())
     ])
